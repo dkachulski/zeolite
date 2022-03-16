e09edfe9a095d39605e8e257e4279ae89bca0125
Your Name <stanimir.nikolov123@gmail.com>
2022-03-16 00:49:58 +0000

    Layout Progress

---
+++ js/cards-expand.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document
+        .querySelectorAll('.card[data-cardnumber]')
+        .forEach(card => card.addEventListener('click', cardsExpandController));
+});
+
+function cardsExpandController(ev) {
+    const elem = ev.currentTarget;
+
+    if (document.querySelector('.card-opened') && elem !== document.querySelector('.card-opened'))
+        changeContent(elem);
+    else if (!elem.classList.contains('card-opened')) showContent(elem);
+    else if (elem.classList.contains('card-opened')) closeContent(elem);
+
+    function showContent(elem) {
+        // mark card as opened
+        elem.classList.add('card-opened');
+
+        // hide all visible content from before
+        document
+            .querySelectorAll('.card-content-body')
+            .forEach(box => box.classList.add('card-content-hidden'));
+
+        // make content inside box visisble
+        document
+            .querySelector(`.${elem.dataset.cardnumber}-content-body`)
+            .classList.remove('card-content-hidden');
+
+        // show content box
+        document.querySelector('#cards-content-collapse-btn').click();
+    }
+
+    function closeContent(elem) {
+        // mark card as closed
+        elem.classList.remove('card-opened');
+
+        // hide content box
+        document.querySelector('#cards-content-collapse-btn').click();
+    }
+
+    function changeContent(elem) {
+        // swap open cards
+        document.querySelector('.card-opened').classList.remove('card-opened');
+        elem.classList.add('card-opened');
+
+        // swap content inside box
+        document
+            .querySelectorAll('.card-content-body')
+            .forEach(box => box.classList.add('card-content-hidden'));
+
+        document
+            .querySelector(`.${elem.dataset.cardnumber}-content-body`)
+            .classList.remove('card-content-hidden');
+    }
+}
