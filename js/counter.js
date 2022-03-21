434de88e425349e627496c87a230238430a6fc00
Your Name <stanimir.nikolov123@gmail.com>
2022-03-21 12:06:57 +0000

    Counter Added (no intersection observer yet)

--- js/counter.js
+++ js/counter.js
@@ -1,23 +1,31 @@
 class Counter {
-    constructor(maxNumber, time) {
-        this.number = 0;
-        this.maxNumber = maxNumber;
+    constructor(number, time, selector) {
+        this.count = 0;
+        this.interval = 20;
+        this.number = number;
         this.time = time;
-        this.speed = time;
+        this.selector = selector;
+        this.rounds = this.time / this.interval;
+        this.increment = number / this.rounds;
     }
 
     start() {
         const self = this;
-        let interval = setInterval(() => {
-            if (number !== this.maxNumber) self.maxNumber += 1;
-            else clearInterval(interval);
-            this.updateDOM();
-        }, 50);
+        self.interval = setInterval(() => {
+            if (self.count < self.number) {
+                self.count += this.increment;
+                self.updateDOM();
+            } else clearInterval(self.interval);
+        }, self.interval);
     }
     updateDOM() {
-        console.log(this.number);
+        document.querySelector(this.selector).textContent = Math.round(this.count);
     }
 }
 
-let counter1 = new Counter(50);
+let counter1 = new Counter(375000, 1500, '#counter-1');
 counter1.start();
+console.log(counter1.increment);
+
+let counter2 = new Counter(1200, 1000, '#counter-2');
+counter2.start();
